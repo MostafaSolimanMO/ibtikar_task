d9bb5bc3d64ee0a9b58b16663c87503534c8235f
Mostafa Soliman <mostafamoma.moma@gmail.com>
2022-07-20 00:39:20 +0200

    fix: image width due the recommendation of the movie API

--- lib/shared/network/api_endpoints.dart
+++ lib/shared/network/api_endpoints.dart
@@ -1,6 +1,6 @@
 class EndPoints {
   static const String baseUrl = 'https://api.themoviedb.org/3/';
-  static const String imageBaseUrl = 'http://image.tmdb.org/t/p/w200/';
+  static const String imageBaseUrl = 'http://image.tmdb.org/t/p/w500/';
 
   static const String token = '8bb5123c4a792f06e0eb0ffa6c6323fb';
 
